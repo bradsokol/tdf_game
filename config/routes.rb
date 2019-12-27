3c86a7f0365415ebc11851263aea3be5cce099fe
Brad Sokol <bsokol@gmail.com>
2019-12-27 11:15:53 -0500

    Simple single-page app with two pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'static#index'
-
   namespace :api, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
+
+  get '*page', to: 'static#index', constraints: ->(request) { !request.xhr? && request.format.html? }
+
+  root 'static#index'
 end
