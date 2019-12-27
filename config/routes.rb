7e7a7e7d3737228f4290dabb15b9cb127bd3f7a8
Brad Sokol <bsokol@gmail.com>
2019-12-27 10:53:36 -0500

    Basic React component

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'static#index'
+
   namespace :api, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
