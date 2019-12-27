ef15ea0a46281aad89bed6bb0dacdc19ba6f934b
Brad Sokol <bsokol@gmail.com>
2019-12-27 12:08:17 -0500

    Get things via API call

--- app/controllers/api/things_controller.rb
+++ app/controllers/api/things_controller.rb
@@ -7,6 +7,10 @@ module Api
         {
           name: 'some-thing',
           guid: '7216a073-d51b-496f-8365-be7cc2a05a4e'
+        },
+        {
+          name: 'another-thing',
+          guid: '23fe9feb-c206-4154-9661-cd98d25bdae7'
         }
       ] }.to_json
     end
--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,27 +1,25 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+
+import thunk from 'redux-thunk';
 
 const initialState = {
-  things: [
-    {
-      name: "Thing 1",
-      guid: "c796e3ab-4268-4396-aa0f-db672888222b"
-    },
-    {
-      name: "Thing 2",
-      guid: "68aabd29-cfae-42f9-9146-9f7bbbc19eca"
-    }
-  ]
+  things: []
 };
 
 function rootReducer(state, action) {
   console.log(action.type);
   switch (action.type) {
-    default:
-      return state
+    case "GET_THINGS_SUCCESS":
+      return { things: action.json.things };
   }
+  return state
 }
 
 export default function configureStore() {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk)
+  );
   return store;
 }
