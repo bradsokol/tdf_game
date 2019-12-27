d85d5dc9180fac420ff06e05c44616397480317c
Brad Sokol <bsokol@gmail.com>
2019-12-27 11:49:12 -0500

    Display state from Redux

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,7 +1,16 @@
 import { createStore } from "redux";
 
 const initialState = {
-  things: []
+  things: [
+    {
+      name: "Thing 1",
+      guid: "c796e3ab-4268-4396-aa0f-db672888222b"
+    },
+    {
+      name: "Thing 2",
+      guid: "68aabd29-cfae-42f9-9146-9f7bbbc19eca"
+    }
+  ]
 };
 
 function rootReducer(state, action) {
