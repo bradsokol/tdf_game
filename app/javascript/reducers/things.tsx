dd16270c70ee933b04bdbff68ab7a301b64e76d2
Brad Sokol <bsokol@gmail.com>
2019-12-30 16:00:10 -0500

    Rough in the dashboard component

--- app/javascript/reducers/things.tsx
+++ app/javascript/reducers/things.tsx
@@ -3,13 +3,13 @@ interface Thing {
   guid: string
 };
 
-const initialState = [];
+// const initialState = [];
 
-export function thingsReducer(state = initialState, action): Thing[] {
-  console.log("thingsReducer type:", action.type);
-  switch (action.type) {
-    case "GET_THINGS_SUCCESS":
-      return action.json.things;
-  }
-  return state
-}
+// export function thingsReducer(state = initialState, action): Thing[] {
+//   console.log("thingsReducer type:", action.type);
+//   switch (action.type) {
+//     case "GET_THINGS_SUCCESS":
+//       return action.json.things;
+//   }
+//   return state
+// }
