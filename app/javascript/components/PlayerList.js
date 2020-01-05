da4e3341b2a31378b97375f9d3144f1b06153d7d
Brad Sokol <bsokol@gmail.com>
2020-01-05 16:59:12 -0500

    Show players from server on dashboard

---
+++ app/javascript/components/PlayerList.js
@@ -0,0 +1,55 @@
+import gql from 'graphql-tag';
+import PropTypes from "prop-types"
+import React from "react"
+import { Query } from 'react-apollo';
+import Table from "react-bootstrap/Table"
+
+const GET_PLAYERS = gql`
+  {
+    tours(year: 2019) {
+      gamePlayers {
+        name
+      }
+    }
+  }
+`
+class PlayerList extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Query query={GET_PLAYERS}>
+          {({ data }) => {
+
+            if (!data) {
+              return <div>Loading...</div>
+            }
+
+            const { tours } = data;
+            const players = tours[0].gamePlayers;
+            return (
+              <div className="table-responsive">
+                <Table striped className="table table-striped table-sm">
+                  <thead>
+                    <tr><td/><td>Player</td></tr>
+                  </thead>
+                  <tbody>
+                    {players.map((player, index) => {
+                      return (
+                        <tr key={index}>
+                          <td className="text-right">{index + 1}</td>
+                          <td>{player.name}</td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </Table>
+              </div>
+            );
+          }}
+        </Query>
+      </React.Fragment>
+    );
+  }
+}
+
+export default PlayerList
