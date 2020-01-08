cdc0ab927874e4a781718471a8d3bdf2fe052d84
Brad Sokol <bsokol@gmail.com>
2020-01-07 21:10:59 -0500

    Delete unecessary stuff from tutorials

--- app/javascript/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { thingsReducer } from './things';
-
-const rootReducer = combineReducers({
-  things: thingsReducer,
-});
-
-export default rootReducer;
