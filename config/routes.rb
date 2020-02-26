f1225ab076b69e5a94ec934f4ac585c34f619862
Brad Sokol <bsokol@gmail.com>
2020-02-26 09:03:14 -0500

    Mount Sidekiq web UI in development

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
+
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
 
   post '/graphql', to: 'graphql#execute'
 
