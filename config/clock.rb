aa775b6868e39051962a878aa26bc32f62922f08
Brad Sokol <bsokol@gmail.com>
2020-02-28 10:58:58 -0500

    Queue the results fetch scheduler job

--- config/clock.rb
+++ config/clock.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+require './config/boot'
+require './config/environment'
+
 require 'clockwork'
 
 module Clockwork
   if Rails.env.development?
-    every(10.seconds, 'Get stage results') {}
+    every(10.seconds, 'Get stage results') { ScheduleStageResultsFetchJob.perform_later }
   else
-    every(15.minutes, 'Get stage results') {}
+    every(15.minutes, 'Get stage results') { ScheduleStageResultsFetchJob.perform_later }
   end
 end
