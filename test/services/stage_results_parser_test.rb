9446b238861f28df1a6fe32edce2a903c6f555cc
Brad Sokol <bsokol@gmail.com>
2020-02-29 13:03:00 -0500

    Remove unnecessary stubbing

--- test/services/stage_results_parser_test.rb
+++ test/services/stage_results_parser_test.rb
@@ -4,21 +4,14 @@ require 'test_helper'
 
 class StageResultsParserTest < ActiveSupport::TestCase
   setup do
+    @html = Nokogiri::HTML(html_fixture('stage_results'))
     @stage_result = StageResult.new
-    stub_stage_results
   end
 
   test 'perform parses points and rank' do
-    StageResultsParser.perform(html: Nokogiri::HTML(html_fixture('stage_results')), stage_result: @stage_result)
+    StageResultsParser.perform(html: @html, stage_result: @stage_result)
 
     assert_equal 38, @stage_result.overall_rank
     assert_equal 53, @stage_result.points
-  end
-
-  private
-
-  def stub_stage_results
-    stub_request(:get, 'https://ifarm.nl/cgi-bin/getlines.cgi?DATE=20200226&SEARCH=Eddie%20Merckx')
-      .to_return(status: 200, body: html_fixture('stage_results'))
   end
 end
