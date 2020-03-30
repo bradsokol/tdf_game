6598c59d5218e16efd30c8d0da596c8bb9cc77de
Brad Sokol <bsokol@gmail.com>
2020-03-30 19:31:05 -0400

    Handle missing stage results data

--- app/javascript/components/StageResults.js
+++ app/javascript/components/StageResults.js
@@ -48,48 +48,54 @@ class StageResults extends React.Component {
             }
 
             const { stageResults } = data;
-            stageResults.sort((a, b) => b.points - a.points);
-            let winnerPoints = stageResults[0].points;
+            if (stageResults.length == 0) {
+              return (
+                <div><p>Results not available.</p></div>
+              );
+            } else {
+              stageResults.sort((a, b) => b.points - a.points);
+              let winnerPoints = stageResults[0].points;
 
-            let poolRank = 1;
-            let points = winnerPoints;
-            stageResults[0].poolRank = poolRank;
-            stageResults.slice(1, stageResults.length).forEach(stageResult => {
-              poolRank += 1;
-              if (stageResult.points != points) {
-                stageResult.poolRank = poolRank;
-                points = stageResult.points;
-              }
-            });
+              let poolRank = 1;
+              let points = winnerPoints;
+              stageResults[0].poolRank = poolRank;
+              stageResults.slice(1, stageResults.length).forEach(stageResult => {
+                poolRank += 1;
+                if (stageResult.points != points) {
+                  stageResult.poolRank = poolRank;
+                  points = stageResult.points;
+                }
+              });
 
-            return (
-              <div className="table-responsive">
-                <Table striped className="table table-striped table-sm">
-                  <thead>
-                    <tr>
-                      <th/>
-                      <th className="text-left">Player</th>
-                      <th className="text-right">Points</th>
-                      <th className="text-right">Pool Behind</th>
-                      <th className="text-right">Overall Rank</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {stageResults.map((result, index) => {
-                      return (
-                        <tr key={index}>
-                          <td className="text-right">{result.poolRank}</td>
-                          <td className="text-left">{result.player.name}</td>
-                          <td className="text-right">{result.points}</td>
-                          <td className="text-right">{winnerPoints - result.points}</td>
-                          <td className="text-right">{result.overallRank}</td>
-                        </tr>
-                      )
-                    })}
-                  </tbody>
-                </Table>
-              </div>
-            );
+              return (
+                <div className="table-responsive">
+                  <Table striped className="table table-striped table-sm">
+                    <thead>
+                      <tr>
+                        <th/>
+                        <th className="text-left">Player</th>
+                        <th className="text-right">Points</th>
+                        <th className="text-right">Pool Behind</th>
+                        <th className="text-right">Overall Rank</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {stageResults.map((result, index) => {
+                        return (
+                          <tr key={index}>
+                            <td className="text-right">{result.poolRank}</td>
+                            <td className="text-left">{result.player.name}</td>
+                            <td className="text-right">{result.points}</td>
+                            <td className="text-right">{winnerPoints - result.points}</td>
+                            <td className="text-right">{result.overallRank}</td>
+                          </tr>
+                        )
+                      })}
+                    </tbody>
+                  </Table>
+                </div>
+              );
+            }
           }}
         </Query>
       </React.Fragment>
