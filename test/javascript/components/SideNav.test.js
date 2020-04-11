eec734030404aa70c834864928bdf2e906f73a77
Brad Sokol <bsokol@gmail.com>
2020-04-11 10:42:59 -0400

    Test Title and SideNav components

---
+++ test/javascript/components/SideNav.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { MemoryRouter } from 'react-router';
+import { Link } from 'react-router-dom'
+
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import SideNav from '../../../app/javascript/components/SideNav';
+
+describe('SideNav', () => {
+  it('has links with the correct text', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <SideNav/>
+      </MemoryRouter>
+    );
+
+    const linkText = ['Overall', 'Stages', 'Teams'];
+    expect(wrapper.find(Link).map(link => link.text())).toStrictEqual(linkText);
+  });
+
+  it('has links with the correct paths', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <SideNav/>
+      </MemoryRouter>
+    );
+
+    const linkPaths = ['/overall', '/stages', '/players'];
+    expect(wrapper.find(Link).map(link => link.props().to)).toStrictEqual(linkPaths);
+  });
+});
---
+++ test/javascript/components/Title.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import Title from '../../../app/javascript/components/Title';
+
+describe('Title', () => {
+  it('shows the app title', () => {
+    const wrapper = mount(<Title/>);
+
+    expect(wrapper.find('a').text()).toBe('Avondale Tour de France Pool');
+  });
+});
