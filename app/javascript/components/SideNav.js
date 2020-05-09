ce750e1b34ae617a6d4a8bc947f80342af31ea7b
Brad Sokol <bsokol@gmail.com>
2020-05-09 17:26:15 -0400

    Move initial stage loading from Routes to SideNav

--- app/javascript/components/SideNav.js
+++ app/javascript/components/SideNav.js
@@ -2,28 +2,55 @@ import { Link } from 'react-router-dom'
 import Nav from "react-bootstrap/Nav"
 import React from "react"
 
-class SideNav extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        <div className="container-fluid">
-          <div className="row">
-            <Nav activekey="Overall" className="flex-column col-md-2 d-done d-md-block bg-light sidebar">
-              <Nav.Item>
-                <Link className="nav-link" to='/overall/2019'>Overall</Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Link className="nav-link" to='/stages/2019/3'>Stages</Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Link className="nav-link" to='/players/2019'>Teams</Link>
-              </Nav.Item>
-            </Nav>
-          </div>
-        </div>
-      </React.Fragment>
-    );
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+export const GET_STAGES_QUERY = gql`
+  query getStages($year: Int!) {
+    tours(year: $year) {
+      stages {
+        id
+        number
+        gameStage
+      }
+    }
   }
+`
+
+function SideNav() {
+  const year = 2019;
+  const { loading, error, data } = useQuery(
+    GET_STAGES_QUERY,
+    { variables: { year } }
+  );
+
+  if (loading || error) return <div/>;
+
+  const { tours } = data;
+  const firstGameStage = tours[0].stages
+    .filter(stage => stage.gameStage === true)
+    .sort((a, b) => a.number - b.number )[0];
+  const stagePath = `/stages/2019/${firstGameStage.number}`;
+
+  return (
+    <React.Fragment>
+      <div className="container-fluid">
+        <div className="row">
+          <Nav activekey="Overall" className="flex-column col-md-2 d-done d-md-block bg-light sidebar">
+            <Nav.Item>
+              <Link className="nav-link" to='/overall/2019'>Overall</Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link className="nav-link" to={stagePath}>Stages</Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link className="nav-link" to='/players/2019'>Teams</Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default SideNav
--- test/javascript/components/SideNav.test.js
+++ test/javascript/components/SideNav.test.js
@@ -2,34 +2,95 @@ import React from 'react';
 
 import { MemoryRouter } from 'react-router';
 import { Link } from 'react-router-dom'
-
-import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {act} from 'react-dom/test-utils';
+import { configure, mount } from 'enzyme';
+
+import { MockedProvider } from '@apollo/react-testing';
 
 configure({ adapter: new Adapter() });
 
-import SideNav from '../../../app/javascript/components/SideNav';
+import SideNav, { GET_STAGES_QUERY } from '../../../app/javascript/components/SideNav';
+
+const waitForExpect = require('wait-for-expect');
+
+configure({ adapter: new Adapter() });
 
 describe('SideNav', () => {
-  it('has links with the correct text', () => {
-    const wrapper = mount(
-      <MemoryRouter>
-        <SideNav/>
-      </MemoryRouter>
-    );
+  it('has links with the correct text', async () => {
+    await act(async () => {
+      const wrapper = mount(
+        <MemoryRouter>
+          <MockedProvider mocks={[stagesMock]} addTypename={false}>
+            <SideNav/>
+          </MockedProvider>,
+        </MemoryRouter>
+      );
 
-    const linkText = ['Overall', 'Stages', 'Teams'];
-    expect(wrapper.find(Link).map(link => link.text())).toStrictEqual(linkText);
+      await waitForExpect(() => {
+        wrapper.update();
+      });
+
+      const linkText = ['Overall', 'Stages', 'Teams'];
+      expect(wrapper.find(Link).map(link => link.text())).toStrictEqual(linkText);
+    });
   });
 
-  it('has links with the correct paths', () => {
-    const wrapper = mount(
-      <MemoryRouter>
-        <SideNav/>
-      </MemoryRouter>
-    );
+  it('has links with the correct paths', async () => {
+    await act(async () => {
+      const wrapper = mount(
+        <MemoryRouter>
+          <MockedProvider mocks={[stagesMock]} addTypename={false}>
+            <SideNav/>
+          </MockedProvider>,
+        </MemoryRouter>
+      );
 
-    const linkPaths = ['/overall/2019', '/stages/2019/3', '/players/2019'];
-    expect(wrapper.find(Link).map(link => link.props().to)).toStrictEqual(linkPaths);
+      await waitForExpect(() => {
+        wrapper.update();
+      });
+
+      const linkPaths = ['/overall/2019', '/stages/2019/3', '/players/2019'];
+      expect(wrapper.find(Link).map(link => link.props().to)).toStrictEqual(linkPaths);
+    });
   });
 });
+
+const stagesMock = {
+  request: {
+    query: GET_STAGES_QUERY,
+    variables: {
+      year: 2019,
+    },
+  },
+  result: {
+    data: {
+      tours: [
+        {
+          stages: [
+            {
+              id: 1,
+              number: 1,
+              gameStage: false,
+            },
+            {
+              id: 2,
+              number: 2,
+              gameStage: false,
+            },
+            {
+              id: 3,
+              number: 3,
+              gameStage: true,
+            },
+            {
+              id: 4,
+              number: 4,
+              gameStage: true,
+            },
+          ],
+        },
+      ],
+    },
+  },
+}
