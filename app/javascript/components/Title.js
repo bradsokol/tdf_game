7216733c1aa7a7bf02dd98e9e7fd8aca8b9369c2
Brad Sokol <bsokol@gmail.com>
2020-05-16 17:09:46 -0400

    Make top bar TdF yellow

--- app/javascript/components/Title.js
+++ app/javascript/components/Title.js
@@ -1,15 +1,13 @@
 import React from "react"
 
-class Title extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/overall">Avondale Tour de France Pool</a>
-        </nav>
-      </React.Fragment>
-    );
-  }
+function Title() {
+  return (
+    <>
+      <nav className="navbar-brand fixed-top flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/overall">Avondale Tour de France Pool</a>
+      </nav>
+    </>
+  );
 }
 
 export default Title
