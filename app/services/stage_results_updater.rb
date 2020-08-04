9478c76bdae2a5d4704b04640bb1aec6b749c144
Brad Sokol <bsokol@gmail.com>
2020-08-04 08:49:48 -0400

    Refactor stage results updater from job to service

---
+++ app/services/stage_results_updater.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+class StageResultsUpdater
+  attr_reader :stage
+
+  def perform(stage_id)
+    @stage = Stage.find(stage_id)
+
+    Rails.logger.info("Updating stage results for stage #{stage.date} (id: #{stage.id})")
+
+    players = Player.find(Registration.where(year: stage.tour.year).pluck(:player_id))
+
+    return unless update_stage_results(players, stage)
+
+    Rails.logger.info("Updated stage results for #{players.length} players for stage #{stage.date} (id: #{stage.id})")
+  end
+
+  private
+
+  def update_stage_result(player, stage)
+    overall_result = OverallResult.find_or_create_by(player_id: player.id, tour_id: stage.tour.id)
+
+    # TODO: Fix this hack
+    @save_overall = stage.date > overall_result.date
+
+    stage_result = StageResult.find_or_create_by(player_id: player.id, stage_id: stage.id)
+    StageResultsFetcher.perform(
+      stage_date: stage.date,
+      player_name: player.name,
+      overall_result: overall_result,
+      stage_result: stage_result
+    )
+    overall_result.save! if @save_overall
+    stage_result.save!
+
+    update_team_result(player, stage_result, overall_result)
+  end
+
+  def update_stage_results(players, stage)
+    StageResult.transaction do
+      players.each do |player|
+        update_stage_result(player, stage)
+      end
+
+      stage.update!(results_downloaded_at: Time.now.utc)
+    end
+    true
+  rescue StandardError => e
+    Rails.logger.error("Failed to update stage result: #{e.message}\n#{e.backtrace.join("\n")}")
+    false
+  end
+
+  def update_team_result(player, stage_result, overall_result)
+    results = TeamResultsFetcher.perform(year: stage.tour.year, player_name: player.name)
+
+    results.riders.each do |rider_results|
+      rider = stage.tour.riders.find_or_create_by(name: rider_results.name)
+      player_rider_points = overall_result.player_rider_points.find_or_create_by(rider: rider)
+      player_rider_points.update!(ordinal: rider_results.ordinal, points: rider_results.total_points)
+
+      player_rider_stage_points = stage_result.player_rider_stage_points.find_or_create_by(rider: rider)
+      player_rider_stage_points.update!(points: rider_results.stage_points[stage.number])
+    end
+
+    stage_result.update!(percentile: results.stage_percentiles[stage.number])
+    overall_result.update!(percentile: results.overall_percentile) if @save_overall
+  end
+end
---
+++ test/services/stage_results_updater_test.rb
@@ -0,0 +1,190 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class StageResultsUpdaterTest < ActiveSupport::TestCase
+  setup do
+    stub_stage_results
+    stub_team_results
+
+    @stage = stages(:tdf_2019_4)
+    @tour = @stage.tour
+  end
+
+  test '#perform fetches results for stage' do
+    player_count = Registration.where(year: @tour.year).count
+    player_rider_points_counter = lambda do
+      @tour.reload.overall_results.reduce(0) { |count, result| count + result.player_rider_points.count }
+    end
+    player_rider_stage_points_counter = lambda do
+      @stage.reload.stage_results.reduce(0) { |count, result| count + result.player_rider_stage_points.count }
+    end
+
+    record_counts = {
+      -> { @stage.stage_results.count } => 2,
+      -> { @tour.overall_results.count } => 0,
+      player_rider_points_counter => player_count * 15,
+      player_rider_stage_points_counter => player_count * 15,
+      -> { @tour.riders.count } => 15
+    }
+
+    assert_difference record_counts do
+      StageResultsUpdater.new.perform(@stage.id)
+    end
+  end
+
+  test '#perform saves stage results' do
+    StageResultsUpdater.new.perform(@stage.id)
+
+    results = @stage.stage_results.first
+    assert_equal 38, results.overall_rank
+    assert_equal 'Jim Hopper', results.player.name
+    assert_equal 52, results.percentile
+    assert_equal 53, results.points
+  end
+
+  test '#perform saves overall results' do
+    stub_stage_results(date: '20190712')
+    @stage.update!(date: Date.new(2019, 7, 12))
+    StageResultsUpdater.new.perform(@stage.id)
+
+    results = @tour.overall_results.last
+    assert_equal Date.new(2019, 7, 12), results.date
+    assert_equal(-41, results.gap)
+    assert_equal 28, results.overall_rank
+    assert_equal 28, results.previous_rank
+    assert_equal 'Will Byers', results.player.name
+    assert_equal 89, results.percentile
+    assert_equal 176, results.points
+  end
+
+  test '#perform saves player rider points' do
+    StageResultsUpdater.new.perform(@stage.id)
+
+    points = overall_results(:tdf_2019_jim_hopper).player_rider_points.last
+    assert_equal 15, points.ordinal
+    assert_equal 0, points.points
+    assert_equal 'D. Martin', points.rider.name
+  end
+
+  test '#perform saves player rider stage points' do
+    StageResultsUpdater.new.perform(@stage.id)
+
+    points = @stage.stage_results.first.player_rider_stage_points.last
+    assert_equal 0, points.points
+    assert_equal 'D. Martin', points.rider.name
+  end
+
+  test '#perform saves rider' do
+    StageResultsUpdater.new.perform(@stage.id)
+
+    assert_equal 'P. Sagan', @tour.riders.first.name
+    assert_equal 'D. Martin', @tour.riders.last.name
+  end
+
+  test '#perform logs on start and success' do
+    message = "Updating stage results for stage #{@stage.date} (id: #{@stage.id})"
+    Rails.logger.expects(:info).with(message)
+    message = "Updated stage results for 2 players for stage #{@stage.date} (id: #{@stage.id})"
+    Rails.logger.expects(:info).with(message)
+
+    StageResultsUpdater.new.perform(@stage.id)
+  end
+
+  test '#perform updates results updated timestamp' do
+    assert_nil @stage.results_downloaded_at
+
+    StageResultsUpdater.new.perform(@stage.id)
+
+    refute_nil @stage.reload.results_downloaded_at
+  end
+
+  test '#perform creates rider when need' do
+    assert_difference -> { @tour.riders.count } => 15 do
+      StageResultsUpdater.new.perform(@stage.id)
+    end
+  end
+
+  test '#perform does not create rider when it exists' do
+    @tour.riders.create!(name: 'P. Sagan')
+
+    assert_difference -> { @tour.riders.count } => 14 do
+      StageResultsUpdater.new.perform(@stage.id)
+    end
+  end
+
+  test '#perform creates player rider points when need' do
+    assert_difference -> { PlayerRiderPoints.count } => @tour.overall_results.count * 15 do
+      StageResultsUpdater.new.perform(@stage.id)
+    end
+  end
+
+  test '#perform does not create player rider points when it exists' do
+    rider = @tour.riders.create!(name: 'P. Sagan')
+    overall_result = overall_results(:tdf_2019_jim_hopper)
+
+    overall_result.player_rider_points.create!(ordinal: 1, points: 1, rider: rider)
+
+    assert_difference -> { PlayerRiderPoints.count } => (@tour.overall_results.count * 15) - 1 do
+      StageResultsUpdater.new.perform(@stage.id)
+    end
+  end
+
+  test '#perform creates player rider stage points when need' do
+    player_count = Registration.where(year: @tour.year).count
+
+    assert_difference -> { PlayerRiderStagePoints.count } => player_count * 15 do
+      StageResultsUpdater.new.perform(@stage.id)
+    end
+  end
+
+  test '#perform does not create player rider stage points when it exists' do
+    StageResultsUpdater.new.perform(@stage.id)
+    @stage.stage_results.first.player_rider_stage_points.first.destroy
+
+    assert_difference -> { PlayerRiderStagePoints.count } => 1 do
+      StageResultsUpdater.new.perform(@stage.id)
+    end
+  end
+
+  test '#perform makes no changes on error' do
+    StageResultsFetcher.expects(:perform).raises(StandardError, 'Boom!')
+
+    record_counts = [
+      -> { StageResult.count },
+      -> { OverallResult.count },
+      -> { PlayerRiderPoints.count },
+      -> { PlayerRiderStagePoints.count },
+      -> { Rider.count }
+    ]
+    assert_no_difference record_counts do
+      StageResultsUpdater.new.perform(@stage.id)
+    end
+
+    assert_nil @stage.reload.results_downloaded_at
+  end
+
+  test '#perform logs on error' do
+    StageResultsFetcher.expects(:perform).raises(StandardError, 'Boom!')
+    message = /^Failed to update stage result: Boom!.*/
+    Rails.logger.expects(:error).with(regexp_matches(message))
+
+    StageResultsUpdater.new.perform(@stage.id)
+  end
+
+  private
+
+  def stub_stage_results(date: '20190709')
+    stub_request(:get, "https://ifarm.nl/cgi-bin/getlines.cgi?DATE=#{date}&SEARCH=Jim%20Hopper")
+      .to_return(status: 200, body: html_fixture('stage_results'))
+    stub_request(:get, "https://ifarm.nl/cgi-bin/getlines.cgi?DATE=#{date}&SEARCH=Will%20Byers")
+      .to_return(status: 200, body: html_fixture('stage_results'))
+  end
+
+  def stub_team_results
+    stub_request(:get, 'https://ifarm.nl/cgi-bin/getpart.cgi?SEARCH=Jim%20Hopper&YEAR=2019')
+      .to_return(status: 200, body: html_fixture('team_results'))
+    stub_request(:get, 'https://ifarm.nl/cgi-bin/getpart.cgi?SEARCH=Will%20Byers&YEAR=2019')
+      .to_return(status: 200, body: html_fixture('team_results'))
+  end
+end
