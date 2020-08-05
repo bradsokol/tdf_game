61f756ff3ba69fed8c2437971c52a4830dd7f2a5
Brad Sokol <bsokol@gmail.com>
2020-08-04 21:27:51 -0400

    Style fixes

--- test/controllers/overall_controller_test.rb
+++ test/controllers/overall_controller_test.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class OverallControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get overall_index_url
     assert_response :success
   end
-
 end
