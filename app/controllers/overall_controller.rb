ad1d2f803c9326e2db4d3cfe0badc5153d305dd9
Brad Sokol <bsokol@gmail.com>
2020-08-04 21:18:00 -0400

    Handle tour not found more gracefully

--- app/controllers/overall_controller.rb
+++ app/controllers/overall_controller.rb
@@ -1,16 +1,25 @@
 # frozen_string_literal: true
 
 class OverallController < ApplicationController
+  def default
+    redirect_to action: 'index', year: most_recent_tour_with_results.year
+  end
+
   def index
     year = params[:year].to_i
     tour = Tour.find_by(year: year)
 
     if tour&.overall_results.blank?
-      year = Tour.all.order(:year).first.year
-      redirect_to action: 'index', year: year
+      redirect_to action: 'index', year: most_recent_tour_with_results.year
     else
       @overall_results = tour.overall_results.order(points: :desc)
       @date = @overall_results.first.date
     end
+  end
+
+  private
+
+  def most_recent_tour_with_results
+    Tour.all.order(year: :desc).select { |tour| tour.overall_results.present? }.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'overall', to: 'overall#default'
   get 'overall/:year', to: 'overall#index'
 end
