76d0b1368f3fd8244ef4f9223a79ec877cfc2904
Brad Sokol <bsokol@gmail.com>
2020-08-22 10:40:28 -0400

    Stages controller tests

--- test/controllers/stages_controller_test.rb
+++ test/controllers/stages_controller_test.rb
@@ -17,4 +17,29 @@ class StagesControllerTest < ActionDispatch::IntegrationTest
       assert_select 'option[selected=selected]', 'Stage 3 - July  8 - Binche to Épernay - 215 km'
     end
   end
+
+  test '#index redirects to first stage with results in default year when tour not found' do
+    get '/stages/1900/1'
+
+    assert_redirected_to '/stages/2019/3'
+  end
+
+  test '#index redirects to first stage with results in default year when stage not found' do
+    get '/stages/2020/99'
+
+    assert_redirected_to '/stages/2019/3'
+  end
+
+  test '#index shows message if stage is not a game stage' do
+    get '/stages/2019/1'
+
+    assert_select 'p', 'This stage is not part of the game.'
+  end
+
+  test '#index shows messages when results are not available' do
+    get '/stages/2019/4'
+
+    assert_response :success
+    assert @response.body.include?('Stage results are not available. Check back later.')
+  end
 end
