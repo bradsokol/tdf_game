e58555a41be6d0eaca47333c52660cd11efc45aa
Brad Sokol <bsokol@gmail.com>
2022-05-02 20:18:53 -0400

    Move all tour, stage and participant setup to seeds.rb

--- db/migrate/20200830012804_tour_2020_participants_1.rb
+++ db/migrate/20200830012804_tour_2020_participants_1.rb
@@ -1,16 +1,5 @@
 class Tour2020Participants1 < ActiveRecord::Migration[6.0]
   def change
-    names = [
-      'Brad Sokol',
-      'Bill Mackenzie',
-      'Brian Power',
-      'Eli Lee',
-      'Peter Zoras',
-    ]
-
-    names.each do |name|
-      player = Player.find_or_create_by!(name: name)
-      player.registrations.create(year: 2020)
-    end
+    # Moved to seeds.rb
   end
 end
--- db/migrate/20200831122747_tour_2020_participants_2.rb
+++ db/migrate/20200831122747_tour_2020_participants_2.rb
@@ -1,23 +1,5 @@
 class Tour2020Participants2 < ActiveRecord::Migration[6.0]
   def change
-    names = [
-      'Ancient Lejogger',
-      'Bob Labuik',
-      'Coco Colombiano',
-      'Ed Quan',
-      'G. Graveldog',
-      'Keven Fitzpatrick',
-      'Malcolm Owen',
-      'Marc Tohir',
-      'Shelagh McIvor',
-      'Ricky Bobby',
-      'Roger Partington',
-      'Ross Woolford',
-    ]
-
-    names.each do |name|
-      player = Player.find_or_create_by!(name: name)
-      player.registrations.create(year: 2020)
-    end
+    # Moved to seeds.rb
   end
 end
