e2c94b3864c9641ba564390b80c154dd340ab073
Brad Sokol <bsokol@gmail.com>
2023-06-24 10:10:48 -0400

    Fix migration

--- db/migrate/20200829141855_fix_2020_stages.rb
+++ db/migrate/20200829141855_fix_2020_stages.rb
@@ -1,7 +1,7 @@
 class Fix2020Stages < ActiveRecord::Migration[6.0]
   def up
-    tour = Tour.last
-    tour.stages.each do |stage|
+    tour = Tour.where(year: 2020).first
+    tour&.stages&.each do |stage|
       stage.update!(date: stage.date.next_year)
     end
   end
