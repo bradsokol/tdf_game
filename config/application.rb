f38b827445c37f5cf1a235b2fbaf05b04d5e144c
Brad Sokol <bsokol@gmail.com>
2023-12-28 15:10:05 -0500

    Changes for Rails v7.1

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ require 'action_mailer/railtie'
 # require "action_text/engine"
 require 'action_view/railtie'
 # require "action_cable/engine"
-require 'sprockets/railtie'
 require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
@@ -24,20 +23,20 @@ Bundler.require(*Rails.groups)
 
 module TdfGame
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    # config.api_only = true
-
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
     config.time_zone = 'Eastern Time (US & Canada)'
-
-    config.active_job.queue_adapter = :sidekiq
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
